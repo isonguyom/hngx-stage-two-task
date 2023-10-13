232621855bc6ff724246cc36becdef7319261f5f
Martin Isonguyo <52254145+isonguyom@users.noreply.github.com>
2023-10-13 20:07:58 +0100

    Update App.js
    Change basename

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <Router basename="hngx-stage-two-task">
+      <Router basename="moviebox">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/movies/:id" element={<Details />} />
